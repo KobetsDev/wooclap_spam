8fe5d64461898c7e1ed27c3f3f91aa71e2bbaee9
KobetsDev <kobets.dev@gmail.com>
2023-04-19 01:28:39 +0300

    fix

--- spam.py
+++ spam.py
@@ -1,6 +1,7 @@
 import asyncio
 import datetime
 import math
+import os
 import random
 from dataclasses import dataclass
 import sys
@@ -24,6 +25,29 @@ class Wooclap:
     def exception_handler(request, exception):
         print("Request failed", request, exception)
 
+    def generate_token(self) -> str:
+        presentDate = datetime.datetime.now()
+        unix_timestamp = datetime.datetime.timestamp(presentDate)*1000
+        return f"z{math.floor(random.random() * random.random() * unix_timestamp)}"
+
+    async def get_info(self) -> List[dict]:
+        async with aiohttp.ClientSession() as session:
+            req = await session.get(self.api_link, json={'isParticipant': 'true', 'from': 'app'},
+                                    headers={
+                'Content-Type': 'application/json',
+                'Accept': '*/*',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Connection': 'keep-alive',
+                'User-Agent': ua.random,
+                'authorization': f"bearer {self.generate_token()}"
+            }
+            )
+            data = await req.json()
+            self.author_id = data.get('selectedQuestion')
+            if not self.author_id:
+                sys.exc_info('author_id = None')
+            return data.get('questions')[-1]#.get('choices')
+
     async def smoothly(self) -> None:
         async with aiohttp.ClientSession() as session:
             for _ in range(self.number_of_attacks):
@@ -36,52 +60,38 @@ class Wooclap:
                 ) as resp:
                     if resp.status != 200:
                         time.sleep(1)
-
                     data = await resp.json()
-                    print(data)
+                    if data.get('error'):
+                        print(data.get('error').get('message'))
+                        await asyncio.sleep(2)
+                        continue
+                    info = await self.get_info()
+                    answers = info.get('choices')
+                    result = info.get('nbAnswersByChoice')
+                    if _ % 3 == 0:
+                        os.system('cls')
+                        print('_'*20)
+                        for answer in answers:
+                            print(answer.get('choice'), result.get(answer.get('_id')))
+                        
 
-    def generate_token(self) -> str:
-        presentDate = datetime.datetime.now()
-        unix_timestamp = datetime.datetime.timestamp(presentDate)*1000
-        return f"z{math.floor(random.random() * random.random() * unix_timestamp)}"
-
-    def get_info(self) -> List[dict]:
-        req = requests.get(self.api_link, json={'isParticipant': 'true', 'from': 'app'},
-                           headers={
-            'Content-Type': 'application/json',
-            'Accept': '*/*',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Connection': 'keep-alive',
-            'User-Agent': ua.random,
-            'authorization': f"bearer {self.generate_token()}"
-        }
-        ).json()
-        self.author_id = req.get('selectedQuestion')
-        if not self.author_id:
-            sys.exc_info('author_id = None')
-        # print(req.get('questions')[-1].get('choices'))
-        return req.get('questions')[-1].get('choices')
-
-    def start(self):
-        # self.api_link: str = 'https://app.wooclap.com/api/events/' + 'www.wooclap.com/GNTAZP'.split('/')[-1]  #
-        self.api_link = 'https://app.wooclap.com/api/events/' + input('Введите ссылку: ').split('/')[-1]  #
-        answers: List[dict] = self.get_info()
+    async def start(self):
+        # self.api_link = 'https://app.wooclap.com/api/events/' + input('Введите ссылку: ').split('/')[-1]  #
+        self.api_link = 'https://app.wooclap.com/api/events/BBGUOK'
+        info = await self.get_info()
+        answers = info.get('choices')
         for num, answer in enumerate(answers):
             print(f'{num+1})', answer.get('choice'))
-        question_number: int = int(input('Введите номер ответа '))
+        question_number = int(input('Введите номер ответа '))
         self.question_id: str = answers[question_number-1].get('_id')
         n_of_a = input('Введите кол-во голосов(50): ')
         if n_of_a != '':
             self.number_of_attacks: int = int(n_of_a)
-        # type_attack: int = int(input('1) Плавно\n2) Спамом(Beta)\nВведите тип атаки: '))
         type_attack: int = 1
         if type_attack == 1:
-            asyncio.get_event_loop().run_until_complete(self.smoothly())
-        # elif type_attack == 2:
-        #     asyncio.get_event_loop().run_until_complete(self.spam())
+            await self.smoothly()
 
 
-W = Wooclap()
-W.start()
-
-# print(W.get_info())
+if __name__ == '__main__':
+    W = Wooclap()
+    asyncio.get_event_loop().run_until_complete(W.start())
